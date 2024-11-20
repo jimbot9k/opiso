aa057c0d58555d0dc6a0924bea289b290974d52a
James Robins <knockknockitsjimmy@outlook.com>
2024-11-20 23:21:04 +1000

    cors headers refined

--- backend/internal/cors/cors_middleware.go
+++ backend/internal/cors/cors_middleware.go
@@ -7,9 +7,8 @@ import (
 func CorsMiddleware(next http.Handler, corsUrl string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", corsUrl)
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
